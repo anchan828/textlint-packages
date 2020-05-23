bfd9afda523f3f4640d7dd3efefab011fbcfb7e5
anchan828 <anchan828@gmail.com>
2020-05-23 03:51:05 +0000

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const prettierRc = require("./.prettierrc");
 module.exports = {
   env: {
-    node: true,
     jest: true,
+    node: true,
   },
   extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "prettier/@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    sourceType: "module",
     project: "./tsconfig.eslint.json",
+    sourceType: "module",
   },
   plugins: ["@typescript-eslint", "sort-keys-fix"],
   rules: {
-    "prettier/prettier": ["error", prettierRc],
-    "lines-between-class-members": ["error", "always"],
-    "no-useless-constructor": "off",
-    "no-unused-vars": "off",
-    "sort-keys-fix/sort-keys-fix": "error",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/no-useless-constructor": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/camelcase": "off",
+    "lines-between-class-members": ["error", "always"],
+    "no-unused-vars": "off",
+    "no-useless-constructor": "off",
+    "prettier/prettier": ["error", prettierRc],
+    "sort-keys-fix/sort-keys-fix": "error",
   },
 };
