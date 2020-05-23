602748034783b78e3bdcc30cede11231866fdaa1
anchan828 <anchan828@gmail.com>
2020-05-23 03:53:30 +0000

    refactor: use decorator class

--- packages/rule-decorators/src/decorators.ts
+++ packages/rule-decorators/src/decorators.ts
@@ -1,10 +1,10 @@
 import { ASTNodeTypes } from "@textlint/ast-node-types";
 import { TEXTLINT_RULES } from "./constants";
 
-export const TextlintASTNodeType = (type: ASTNodeTypes): Function => {
-  return (target: any, propertyName: string): void => Reflect.defineMetadata(type, propertyName, target);
+export const TextlintASTNodeType = (type: ASTNodeTypes): MethodDecorator => {
+  return (target: object, propertyName: string | symbol): void => Reflect.defineMetadata(type, propertyName, target);
 };
 
-export const TextlintRule = (): Function => {
+export const TextlintRule = (): ClassDecorator => {
   return (target: object): void => Reflect.defineMetadata(TEXTLINT_RULES, true, target);
 };
